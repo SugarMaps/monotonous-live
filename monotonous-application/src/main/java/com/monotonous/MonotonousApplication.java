c87fb43c5ffa3c7175b72d84899feb819acbafb6
beats <yaqiang.yao@hipac.cn>
2020-08-22 16:46:49 +0800

    feat: 依赖

--- monotonous-application/src/main/java/com/monotonous/MonotonousApplication.java
+++ monotonous-application/src/main/java/com/monotonous/MonotonousApplication.java
@@ -1,6 +1,5 @@
 package com.monotonous;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,8 +8,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @author beats
  */
 @SpringBootApplication
-@EnableAspectJAutoProxy
-@MapperScan("com.monotonous.repository.mapper")
+//@EnableAspectJAutoProxy
+//@MapperScan("com.monotonous.repository.mapper")
 public class MonotonousApplication {
 
     public static void main(String[] args) {
